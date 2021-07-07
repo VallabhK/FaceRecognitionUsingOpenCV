d5cc2ca1e8bbc4478291c7ad8936c453a0bc8d81
Vallabhk <vallabhkaranjkar@outlook.com>
2021-07-07 21:58:40 +0530

    Added changes for preprocesing

--- CreateEmbeddings.py
+++ CreateEmbeddings.py
@@ -36,3 +36,25 @@ detector = cv2.dnn.readNetFromCaffe(protoPath, modelPath)
 print("[INFO] loading face recognizer...")
 embedder = cv2.dnn.readNetFromTorch(args["embedding_model"])
 
+# grab the paths to the input images in our dataset
+print("[INFO] quantifying faces...")
+imagePaths = list(paths.list_images(args["dataset"]))
+# initialize our lists of extracted facial embeddings and
+# corresponding people names
+knownEmbeddings = []
+knownNames = []
+# initialize the total number of faces processed
+total = 0
+
+# loop over the image paths
+for (i, imagePath) in enumerate(imagePaths):
+	# extract the person name from the image path
+	print("[INFO] processing image {}/{}".format(i + 1,
+		len(imagePaths)))
+	name = imagePath.split(os.path.sep)[-2]
+	# load the image, resize it to have a width of 600 pixels (while
+	# maintaining the aspect ratio), and then grab the image
+	# dimensions
+	image = cv2.imread(imagePath)
+	image = imutils.resize(image, width=600)
+	(h, w) = image.shape[:2]
