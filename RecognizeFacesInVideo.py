969d364c6b0005664f6d979521de78ff3f99f56f
Vallabhk <vallabhkaranjkar@outlook.com>
2021-07-08 23:14:43 +0530

    Added the file for video input

---
+++ RecognizeFacesInVideo.py
@@ -0,0 +1,28 @@
+#This is an implementation which deals with the video input
+#The pipeline remains the same for recognizing faces in the images
+#Additional part is capturing single frames from video and feeding them to the recognition engine
+
+# import the necessary packages
+from imutils.video import VideoStream
+from imutils.video import FPS
+import numpy as np
+import argparse
+import imutils
+import pickle
+import time
+import cv2
+import os
+# construct the argument parser and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-d", "--detector", required=True,
+	help="path to OpenCV's deep learning face detector")
+ap.add_argument("-m", "--embedding-model", required=True,
+	help="path to OpenCV's deep learning face embedding model")
+ap.add_argument("-r", "--recognizer", required=True,
+	help="path to model trained to recognize faces")
+ap.add_argument("-l", "--le", required=True,
+	help="path to label encoder")
+ap.add_argument("-c", "--confidence", type=float, default=0.5,
+	help="minimum probability to filter weak detections")
+args = vars(ap.parse_args())
+
